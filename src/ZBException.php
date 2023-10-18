d26eb5c9d369e865a96c3a837433528cd15bedfb
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-10-18 14:13:01 +0200

    Split exceptions classes

--- src/ZBException.php
+++ src/ZBException.php
@@ -7,15 +7,3 @@ use Exception;
 class ZBException extends Exception
 {
 }
-
-class ZBMissingApiKeyException extends ZBException
-{
-}
-
-class ZBMissingParameterException extends ZBException
-{
-}
-
-class ZBApiException extends ZBException
-{
-}
